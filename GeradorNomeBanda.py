2916c5b6ca911bf0a3e5267f6076602d99b61004
Gabriel Felipe Kaviatkoski Coradin <84419750+Gkacor@users.noreply.github.com>
2022-06-21 02:39:25 -0300

    Projeto1

---
+++ GeradorNomeBanda.py
@@ -0,0 +1,13 @@
+#1. Create a greeting for your program.
+print("Olá! Esse é o gerador de nomes de banda.")
+
+#2. Ask the user for the city that they grew up in.
+cidade = input("Vamos lá, qual é o nome da cidade que você nasceu?\n")
+
+
+#3. Ask the user for the name of a pet.
+pet = input("Agora, qual é o nome do seu animal de estimação?\n")
+
+
+#4. Combine the name of their city and pet and show them their band name.
+print("O nome da sua banda pode ser " + cidade + " " + pet)
